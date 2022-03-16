ca21f02e8545b475566871b31885c47d45460469
Prakhar Gupta <guptaprakhar272@gmail.com>
2022-03-16 16:51:15 +0530

    Comments added

--- server.py
+++ server.py
@@ -11,10 +11,10 @@ It provides classes, instance variables & methods that can be used by subclass.
 class ServerRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         """ HTTP Response = Response + Headers + Body(containing the resource to be share in bytes)"""
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        no_of_bytes = self.wfile.write(bytes("Hello World","UTF-8"))
+        self.send_response(200) # Adds response type (200 OK) to the HTTP Response
+        self.send_header('Content-type', 'text/html') # Adds headers to the HTTP Response
+        self.end_headers() # Ends headers
+        no_of_bytes = self.wfile.write(bytes("Hello World","UTF-8")) # Adds content to be send to the HTTP Response
         print(no_of_bytes)
 
     def do_POST(self):
